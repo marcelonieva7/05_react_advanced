882184e6d008f7eb1c88172ca75583ae26d27228
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2024-07-10 05:47:17 +0000

    feat: add router provider

---
+++ src/libs/router/routerProvider.tsx
@@ -0,0 +1,12 @@
+import {
+    RouterProvider as LibraryRouterProvider,
+    createBrowserRouter,
+    type RouteObject
+} from 'react-router-dom';
+  
+  const RouterProvider = ({ routes }: { routes: RouteObject[] }) => (
+    <LibraryRouterProvider router={createBrowserRouter(routes)} />
+  );
+  
+  export { RouterProvider };
+  
