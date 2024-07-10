05f5fde26f42427d91a6af8debaed7ac1f6fd07f
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2024-07-10 23:42:02 +0000

    refactor: remove bad identation

--- src/libs/router/routerProvider.tsx
+++ src/libs/router/routerProvider.tsx
@@ -1,12 +1,11 @@
 import {
-    RouterProvider as LibraryRouterProvider,
-    createBrowserRouter,
-    type RouteObject
+  RouterProvider as LibraryRouterProvider,
+  createBrowserRouter,
+  type RouteObject
 } from 'react-router-dom';
   
-  const RouterProvider = ({ routes }: { routes: RouteObject[] }) => (
-    <LibraryRouterProvider router={createBrowserRouter(routes)} />
-  );
-  
-  export { RouterProvider };
-  
+const RouterProvider = ({ routes }: { routes: RouteObject[] }) => (
+  <LibraryRouterProvider router={createBrowserRouter(routes)} />
+);
+
+export { RouterProvider };
