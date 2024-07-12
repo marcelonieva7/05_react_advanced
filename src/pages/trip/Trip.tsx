e2ff198b608dc52d3ef0a1235d10ec11399fc984
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-12 20:47:31 -0300

    feat: add count down redirect

--- src/pages/trip/Trip.tsx
+++ src/pages/trip/Trip.tsx
@@ -7,22 +7,29 @@ import { AppRoutes } from "@/libs/router/appRoutes";
 import styles from './styles/trip.module.css'
 import trips from '@/assets/data/trips.json';
 
-const TripNotFound = () => (
+const TripNotFound: FC<{ count: number}> = ({ count }) => (
   <>
     <h2>Trip not found</h2>
-    <h3>Redirecting to the main page</h3>
+    <h3>Redirecting to the main page in {count} seconds</h3>
   </>
 )
 
 const Trip:FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [count, setCount] = useState(5);
   const navigate = useNavigate();
   const { id } = useParams();
   const trip = trips.find(trip => trip.id === id);
 
   useEffect(() => {
     if (!trip) {
-      setTimeout(() => navigate(AppRoutes.HOME), 5000)
+      const timeout = setTimeout(() => navigate(AppRoutes.HOME), 5000)      
+      const interval = setInterval(() => setCount(prev => prev - 1), 1000)
+
+      return () => {
+        clearInterval(interval)
+        clearTimeout(timeout)
+      }
     }
   }, [navigate, trip]);
   
@@ -34,7 +41,7 @@ const Trip:FC = () => {
           <TripInfo {...trip} setIsOpen={setIsOpen} />
           <BookTripModal isOpen={isOpen} setIsOpen={setIsOpen} trip={trip} />
         </>
-      ) : <TripNotFound />}
+      ) : <TripNotFound count={count} />}
     </main>
   )
 }
