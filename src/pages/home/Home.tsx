3a0d90bc89c9f49d9c537031e56d8c6c66c1797c
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2024-07-12 20:08:21 +0000

    refactor: add home page folder

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,59 @@
+import { type FC, useState } from 'react';
+import { TripsFilter } from '@/components/tripsFilter/tripsFilter';
+import { Tripcard } from '@/components/tripCard/tripCard'
+
+import styles from './styles/home.module.css'
+import trips from '@/assets/data/trips.json';
+
+interface Trip {
+  id: string;
+  title: string;
+  description: string;
+  level: string;
+  duration: number;
+  price: number;
+  image: string;
+  createdAt: string;
+}
+
+const Home: FC = () => {
+  const [filteredTrips, setFilteredTrips] = useState<Trip[]>(trips);
+
+  const handleFilterChange = (filters: { search: string; duration: string; level: string }) => {
+    const { search, duration, level } = filters;
+
+    const filtered = trips.filter((trip) => {
+      const matchesSearch = trip.title.toLowerCase().includes(search.toLowerCase());
+      const matchesLevel = !level || trip.level === level;
+
+      let matchesDuration = true;
+
+      if (duration) {
+        const [min, max] = duration.split('_x_').map(Number);
+        if (max) {
+          matchesDuration = trip.duration >= min && trip.duration <= max;
+        } else {
+          matchesDuration = trip.duration >= min;
+        }
+      }
+
+      return matchesSearch && matchesLevel && matchesDuration;
+    });
+
+    setFilteredTrips(filtered);
+  };
+
+  return (
+    <main>
+      <TripsFilter onFilterChange={handleFilterChange} />
+      <section className={styles["trips"]}>
+        <h2 className="visually-hidden">Trips List</h2>
+        <ul className={styles["trip-list"]}>
+          {filteredTrips.map(trip => <Tripcard {...trip} key={trip.id} />)}
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+export { Home };
---
+++ src/pages/home/styles/home.module.css.d.ts
@@ -0,0 +1,1 @@
+declare const styles: {readonly 'trips': string;readonly 'trip-list': string;};export default styles;
