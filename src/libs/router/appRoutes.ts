4ccdcf2ac9e8adbca89efb8e2961aa4c7e2dfc13
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2024-07-12 20:09:30 +0000

    feat: add new routes

--- src/libs/router/appRoutes.ts
+++ src/libs/router/appRoutes.ts
@@ -2,7 +2,8 @@ const AppRoutes = {
   ROOT: '/',
   HOME: '/',
   BOOKINGS: '/bookings',
-  SIGN_IN: '/sign-in'
+  SIGN_IN: '/sign-in',
+  TRIP: '/trip'
 } as const satisfies Record<Uppercase<string>, `/${string}`>
 
 export { AppRoutes }
