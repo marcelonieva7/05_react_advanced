69c2ce595b92d37df9622e993775bf639ae11ef0
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-12 23:12:48 -0300

    feat: add signUp route

--- src/libs/router/appRoutes.ts
+++ src/libs/router/appRoutes.ts
@@ -3,6 +3,7 @@ const AppRoutes = {
   HOME: '/',
   BOOKINGS: '/bookings',
   SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
   TRIP: '/trip'
 } as const satisfies Record<Uppercase<string>, `/${string}`>
 
