8dc7eea4dcc1f873ab3bd9bfbcdf83aa5d9b10ef
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-12 22:31:22 -0300

    feat: add booking type

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -9,4 +9,15 @@ type Trip = {
   createdAt: string;
 }
 
-export { type Trip }
+type Booking =   {
+  id: string,
+  userId: string,
+  tripId: string,
+  guests: number,
+  date: string,
+  trip: Pick<Trip, ('title' | 'duration' | 'price')>,
+  totalPrice: number,
+  createdAt: string
+}
+
+export { type Booking, type Trip }
