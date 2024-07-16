3526432684f572489185a91ca7164fdf9e4c5d0f
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2024-07-16 00:34:54 +0000

    feat: implement redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import App from './App.jsx'
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './libs/redux/store.js';
+import App from './App.jsx'
+
 import './index.css'
 
 const root = createRoot(document.querySelector('#root')!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store.instance}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
