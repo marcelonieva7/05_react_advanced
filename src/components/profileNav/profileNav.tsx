f9624cd580ba2be97c798599ce9a10c2eea92f44
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2024-07-16 02:26:12 +0000

    refactor: implement actions on profile

--- src/components/profileNav/profileNav.tsx
+++ src/components/profileNav/profileNav.tsx
@@ -1,7 +1,11 @@
 import { type FC, type HTMLAttributes } from 'react';
 import { Link } from 'react-router-dom';
 import { AppRoutes } from '@/libs/router/appRoutes';
-import user from '@/assets/images/user.svg'
+import { useAppSelector } from '@/hooks/useAppSelector';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { Button } from '../button/button';
+import { authActions } from '@/libs/redux/slices/auth';
+import userImg from '@/assets/images/user.svg'
 
 import styles from './styles/profilenav.module.css';
 import stylesBtn from '@/components/button/styles/button.module.css'
@@ -11,24 +15,44 @@ interface ProfileNavProps extends HTMLAttributes<HTMLDivElement> {
   'data-test-id'?: string;
 }
 
-const ProfileData = {
-  name: "John Doe"
-}
-
 const ProfileNav: FC<ProfileNavProps> = ({ 'data-test-id': dataTestId, className, ...props }) => {
+  const { user } = useAppSelector(({ auth }) => auth);
+  const dispatch = useAppDispatch();
+
+  const handleSignOut = () => {
+    dispatch(authActions.signOut());
+  }
+
   return (
     <div data-test-id={dataTestId} className={[stylesNavLink.navHeader__inner, styles.profileNav, className].join(' ')} tabIndex={0} {...props}>
       <span className="visually-hidden">Profile</span>
-      <img src={user} alt="profile" />
+      <img src={userImg} alt="profile" />
       <ul data-test-id="header-profile-nav-list" className={styles.profileNav__list}>
-        <li data-test-id="header-profile-nav-username" className={styles.profileNav__item}>
-          {ProfileData.name}
-        </li>
-        <li className={styles.profileNav__item}>
-          <Link data-test-id="header-profile-nav-sign-out" to={AppRoutes.SIGN_IN} className={[styles.profileNav__signOut, stylesBtn.button].join(' ')}>
-            Sign Out
-          </Link>
-        </li>
+        {user ? (
+          <>
+          <li data-test-id="header-profile-nav-username" className={styles.profileNav__item}>
+            {user.fullName}
+          </li>
+          <li className={styles.profileNav__item}>
+            <Button data-test-id="header-profile-nav-sign-out" className={styles.profileNav__signOut} onClick={handleSignOut}>
+              Sign Out
+            </Button>
+          </li>
+          </>
+        ) : (
+          <>
+          <li className={styles.profileNav__item}>
+            <Link data-test-id="header-profile-nav-sign-in" to={AppRoutes.SIGN_IN} className={[styles.profileNav__signOut, stylesBtn.button].join(' ')}>
+              Sign In
+            </Link>
+          </li>
+          <li className={styles.profileNav__item}>
+            <Link data-test-id="header-profile-nav-sign-up" to={AppRoutes.SIGN_UP} className={[styles.profileNav__signOut, stylesBtn.button].join(' ')}>
+              Sign Up
+            </Link>
+          </li>
+          </>
+        )}
       </ul>
     </div>
   );
