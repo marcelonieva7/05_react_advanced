3c36fee2e3a61a057bb9b71b81b03712ad29f4c1
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-16 20:23:34 -0300

    feat: add toast container

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './libs/redux/store.js';
+import { ToastContainer } from 'react-toastify';
+import { store } from '@/libs/redux/store.js';
 import App from './App.jsx'
 
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
 
 const root = createRoot(document.querySelector('#root')!);
 
 root.render(
   <React.StrictMode>
+    <ToastContainer />
     <Provider store={store.instance}>
       <App />
     </Provider>
