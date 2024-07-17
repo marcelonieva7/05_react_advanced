7f23fe8cf4ffc588bf63b8d7d98c9f9a7f2de89b
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-16 22:30:10 -0300

    refactor: implement redux on Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,41 +1,60 @@
-import { type FC, useState } from 'react';
+import { type FC, useState, useEffect } from 'react';
 import { TripsFilter } from '@/components/tripsFilter/tripsFilter';
 import { Tripcard } from '@/components/tripCard/tripCard'
 import { type Trip } from '@/@types/';
 
 import styles from './styles/home.module.css'
-import trips from '@/assets/data/trips.json';
+import { useAppSelector } from '@/hooks/useAppSelector';
+import { DataStatus } from '@/constants/redux';
+import { Loader } from '@/components/loader/loader';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { tripsActions } from '@/libs/redux/slices/trips';
 
 const Home: FC = () => {
-  const [filteredTrips, setFilteredTrips] = useState<Trip[]>(trips);
+  const { dataStatus, trips } = useAppSelector(({ trips }) => trips);
+  const [ filteredTrips, setFilteredTrips ] = useState<Trip[]>([]);
+  const isLoading = dataStatus === DataStatus.PENDING;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(tripsActions.getAllTrips());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (trips) {
+      setFilteredTrips(trips);
+    }
+  }, [trips]);
 
   const handleFilterChange = (filters: { search: string; duration: string; level: string }) => {
     const { search, duration, level } = filters;
 
-    const filtered = trips.filter((trip) => {
-      const regex = new RegExp(search, 'i');
-      const matchesSearch = trip.title.search(regex) !== -1;
-      const matchesLevel = !level || trip.level === level;
+    if (trips) {
+      const filtered = trips.filter((trip) => {
+        const regex = new RegExp(search, 'i');
+        const matchesSearch = trip.title.search(regex) !== -1;
+        const matchesLevel = !level || trip.level === level;
 
-      let matchesDuration = true;
+        let matchesDuration = true;
 
-      if (duration) {
-        const [min, max] = duration.split('_x_').map(Number);
+        if (duration) {
+          const [min, max] = duration.split('_x_').map(Number);
 
-        if (max) {
-          matchesDuration = trip.duration > min && trip.duration <= max;
-        } else {
-          matchesDuration = trip.duration > min;
+          if (max) {
+            matchesDuration = trip.duration > min && trip.duration <= max;
+          } else {
+            matchesDuration = trip.duration > min;
+          }
         }
-      }
-
-      return matchesSearch && matchesLevel && matchesDuration;
-    });
-
-    setFilteredTrips(filtered);
+        return matchesSearch && matchesLevel && matchesDuration;
+      });
+      setFilteredTrips(filtered)
+    }
   };
 
-  return (
+  return isLoading ?
+    <Loader />
+    : (
     <main>
       <h1 className="visually-hidden">Travel App</h1>
       <TripsFilter onFilterChange={handleFilterChange} />
