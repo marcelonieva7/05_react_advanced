74fb6c15b23e26736627bd0364a25f056f2542df
Marcelo Nieva <marcelonieva7@gmail.com>
2024-07-17 01:13:56 -0300

    feat: implement redux to handle async on Bookings Page

--- src/components/booking/booking.tsx
+++ src/components/booking/booking.tsx
@@ -1,25 +1,32 @@
-import { useState, type FC } from "react";
+import { type FC } from "react";
 import { type Booking } from "@/@types";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { bookingsActions } from "@/libs/redux/slices/bookings";
+import { DataStatus } from "@/constants/redux";
+import { notification } from "@/libs/notification/notificationService";
+import { useAppSelector } from "@/hooks/useAppSelector";
 
 import styles from './styles/booking.module.css'
 
 interface BookingProps {
-  setBookings: React.Dispatch<React.SetStateAction<Booking[]>>;
   booking: Booking;
 }
 
-const Booking: FC<BookingProps> = ({ setBookings, booking }) => {
+const Booking: FC<BookingProps> = ({ booking }) => {
   const { guests, trip, date, totalPrice, id } = booking
-  const [isDeleting, setIsDeleting] = useState(false);
+  const dispatch = useAppDispatch();
+  const { dataStatus } = useAppSelector(({ bookings }) => bookings)
+  const isLoading = dataStatus === DataStatus.PENDING;
 
-  const handleCancel = (): void => {
-    setIsDeleting(true);
-    setTimeout(() => {
-      setBookings(prev => prev.filter(booking => booking.id !== id));
-    }, 500);
+  const handleCancelBooking = (): void => {
+    dispatch(bookingsActions.deleteBooking({ bookingId: id })).then( action => {
+      if (action.meta.requestStatus === DataStatus.FULFILLED) {
+        notification.success("Booking deleted successfully");
+      }
+    })
   }
   return (
-    <li data-test-id="booking" className={`${styles["booking"]} ${isDeleting ? styles["booking--deleting"] : ""}`}>
+    <li data-test-id="booking" className={styles["booking"]}> {/* ${isDeleting ? styles["booking--deleting"] : ""} */}
       <h3 data-test-id="booking-title" className={styles["booking__title"]}>{trip.title}</h3>
       <span data-test-id="booking-guests" className={styles["booking__guests"]}>
         {`${guests} guests`}
@@ -33,11 +40,12 @@ const Booking: FC<BookingProps> = ({ setBookings, booking }) => {
       <button
         data-test-id="booking-cancel"
         className={styles["booking__cancel"]}
-        onClick={handleCancel}
+        onClick={handleCancelBooking}
         title="Cancel booking"
+        disabled={isLoading}
       >
         <span className="visually-hidden">Cancel booking</span>
-        ×
+          {isLoading ? "..." : "×"}
       </button>
     </li>
   )
--- src/pages/bookings/bookings.tsx
+++ src/pages/bookings/bookings.tsx
@@ -1,24 +1,31 @@
-import { type FC } from "react";
+import { useEffect, type FC } from "react";
 import { Booking } from "@/components/booking/booking";
-import { type Booking as TBooking } from "@/@types";
+import { useAppSelector } from "@/hooks/useAppSelector";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { bookingsActions } from "@/libs/redux/slices/bookings";
+import { Loader } from "@/components/loader/loader";
 
 import styles from './styles/bookings.module.css'
 
-interface BookingsProps {
-  setBookings: React.Dispatch<React.SetStateAction<TBooking[]>>;
-  bookings: TBooking[];
-}
+const Bookings:FC = () => {
+  const { bookings, isGettingBookings} = useAppSelector(({ bookings }) => bookings)
+  const dispatch = useAppDispatch();
 
-const Bookings:FC<BookingsProps> = ({ setBookings, bookings }) => {
-  const sortedBookings = bookings.sort((a, b) => {
+  useEffect(() => {
+    dispatch(bookingsActions.getAllBookings());
+  }, [dispatch]);
+
+  const sortedBookings = [...bookings].sort((a, b) => {
     return new Date(a.date).valueOf() - new Date(b.date).valueOf()
   })
-  return (
+  return isGettingBookings ?
+    <Loader />
+    : (
     <main className={styles["bookings-page"]}>
       <h1 className="visually-hidden">Travel App</h1>
       <ul className={styles["bookings__list"]}>
         {sortedBookings.length > 0 ?
-          sortedBookings.map(booking => <Booking key={booking.id} booking={booking} setBookings={setBookings} />)
+          sortedBookings.map(booking => <Booking key={booking.id} booking={booking}/>)
           : 
           <h1 className={styles["bookings__empty"]}>No bookings yet</h1>
         }
